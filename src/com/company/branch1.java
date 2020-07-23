fbe6147c86d5a3ae7d107593c2a154f08cbce807
sgurung7s <64933110+sgurung7s@users.noreply.github.com>
2020-07-23 12:35:44 -0500

    branch1u

--- src/com/company/branch1.java
+++ src/com/company/branch1.java
@@ -3,6 +3,10 @@ package com.company;
 import java.util.*;
 
 public class branch1 {
-    System.out.println("This is branch1");
+    public static void main(String[] args) {
+        System.out.println("This is branch1");
+
+    }
+
 
 }
---
+++ src/com/company/branch2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+
+public class branch2 {
+    public static void main(String[] args) {
+        System.out.println("This is branch2");
+
+    }
+
+}
